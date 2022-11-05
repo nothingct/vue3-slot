0ffcc23606f853971ffcb2906632787116822383
nothingct <nothingct@naver.com>
2022-11-06 02:42:19 +0900

    vue3-slot

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <MyBtn>
+    <template #icon>
+      <span>(H)</span>
+    </template>
+    <template #text>
+      <span>Here Comes The Life.</span>
+    </template>
+  </MyBtn>  
+</template>
+<script>
+
+import MyBtn from '~/components/MyBtn'
+export default {
+  components:{
+    MyBtn
+  }
+}
+</script>
